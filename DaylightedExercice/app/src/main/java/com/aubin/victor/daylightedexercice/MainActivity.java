d9b9a424fdc729d2595dc1b8a5bcc8f563f72b31
Victor Aubin <victor.aubin@student.ecp.fr>
2017-05-10 15:56:30 +0200

    Test Http Request

--- DaylightedExercice/app/src/main/java/com/aubin/victor/daylightedexercice/MainActivity.java
+++ DaylightedExercice/app/src/main/java/com/aubin/victor/daylightedexercice/MainActivity.java
@@ -1,13 +1,73 @@
 package com.aubin.victor.daylightedexercice;
 
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+    TextView mTxtDisplay;
+    ImageView mImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mTxtDisplay = (TextView) findViewById(R.id.txtDisplay);
+        volleyJsonArrayRequest("https://www.daylighted.com/corporate/collection/add/1d85fb9e2f5511e78b047054d219b30e?format=json&f=71&p=1");
     }
+
+    public void volleyJsonArrayRequest(String url){
+
+        String  REQUEST_TAG = "com.androidtutorialpoint.volleyJsonArrayRequest";
+
+        JsonArrayRequest jsonArrayReq = new JsonArrayRequest(url,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Log.d("TAG", response.toString());
+                        LayoutInflater li = LayoutInflater.from(MainActivity.this);
+                        //showDialogView = li.inflate(R.layout.show_dialog, null);
+                        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
+                        //alertDialogBuilder.setView(showDialogView);
+                        alertDialogBuilder
+                                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                    }
+                                })
+                                .setCancelable(false)
+                                .create();
+                        mTxtDisplay.setText(response.toString());
+                        alertDialogBuilder.show();
+                        //progressDialog.hide();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d("TAG", "Error: " + error.getMessage());
+                mTxtDisplay.setText(error.getMessage());
+                //progressDialog.hide();
+            }
+        });
+
+        // Adding JsonObject request to request queue
+        AppSingleton.getInstance(getApplicationContext()).addToRequestQueue(jsonArrayReq, REQUEST_TAG);
+    }
+
 }
